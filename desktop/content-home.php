3dfb883bb7b600152e4055186f55e46e71d404a9
tekleang <tekleangtomnerb@gmail.com>
2019-01-02 11:43:47 +0700

    first commit

---
+++ desktop/content-home.php
@@ -0,0 +1,403 @@
+<div id="wrapper">
+		<div id="content">
+
+<style type="text/css">
+.carousel-caption .text{display: flex;justify-content: center;flex-direction: column;align-items: center;}
+
+.carousel-caption .text h1{padding: 0 15%;}
+}
+</style>
+			<!-- banner home -->
+			<div class="container-fluid banner-home">
+				  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+				    <!-- Indicators -->
+				    <ol class="carousel-indicators">
+				      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				      <li data-target="#myCarousel" data-slide-to="1"></li>
+				      <li data-target="#myCarousel" data-slide-to="2"></li>
+				    </ol>
+
+				    <!-- Wrapper for slides -->
+
+			    <div class="carousel-inner">
+			    	<div class="box-img" style="position: absolute;z-index: 111;width: 100%;height: 100%;display: flex;'justify-content: center;align-items: center;">
+	              		<div class="img" style="display: flex;justify-content: center;align-items: center;width: 100%;position: absolute;top: 10px;">
+	              			<img src="assets/images/Architecture-option21-logo.svg" >
+	              		</div>
+					</div>	
+			      <div class="item active banner-img" data-fullscreen>
+			      	  <div class="gradientb"></div>
+		      		  <img data-imgzoom data-sarallax src="assets/images/Architecture-homepage-Link-option21-banner-01.jpg" alt="Architecture">
+			          <div class="carousel-caption">
+			              <div class="text">
+			              	<div class="linehigh"></div>
+			              	<h1>ARCHSITE FROM SKETCHES TO REAL LIFE</h1>
+			              </div>			          	
+			          </div>			        
+			      </div>
+
+			      <div class="item banner-img" data-fullscreen>
+			      	<div class="gradientb" ></div>
+			        <img data-imgzoom data-sarallax src="assets/images/Architecture-homepage-Link-option21-banner-01.jpg" alt="Architecture">
+			          <div class="carousel-caption">
+			          	  		          	
+			          </div>
+			      </div>
+
+			      <div class="item banner-img" data-fullscreen>
+			      	<div class="gradientb" ></div>
+			        <img data-imgzoom data-sarallax src="assets/images/Architecture-homepage-Link-option21-banner-01.jpg" alt="Architecture">
+			          <div class="carousel-caption">
+			          			          	
+			          </div>
+			      </div>
+
+			    </div>
+
+			    <!-- Left and right controls -->
+			    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			      <span class="glyphicon glyphicon-chevron-left"></span>
+			      <span class="sr-only">Previous</span>
+			    </a>
+			    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+			      <span class="glyphicon glyphicon-chevron-right"></span>
+			      <span class="sr-only">Next</span>
+			    </a>
+			  </div>
+			</div>
+<style type="text/css">
+	/*We build our projects with your dreams and ideas.*/
+#content .build{position: relative; margin-top: 80px;padding-bottom: 80px;}
+#content .build .col-sm-6 .img{margin-top: 50px;height: 575px;}
+#content .build .col-sm-6 .img img{width: 100%;height: 100%;object-fit: cover;}
+#content .build .welcome{position: absolute;top: 0;}
+#content .build .welcome .box-text{}
+#content .build .welcome .box-text h4{margin: 0;font-size: 18px;text-transform: uppercase;letter-spacing: 7px;margin-top: 50px;}
+#content .build .welcome .box-text p{font-size: 18px;margin-top: 50px;}
+#content .build .welcome .box-text h2{text-align: left;margin: 0;font-size: 35px;margin-top: 50px;padding-right: 2%;}
+
+</style>
+			<!-- We build our projects with your dreams and ideas. -->
+			<div class="container-fuild build">
+				<div class="container" style="width: 75%;">
+					<div class="container welcome">
+						<div class="row">
+							<div class="col-sm-6">
+								<div class="box-text">
+									<div class="linehigh"></div>
+									<h4>WELCOME TO ARCHSITE</h4>
+									<h2>We build our projects with your dreams and ideas.</h2>
+									<p>ARCHSITE is an award-winning group of architects and interior designers based in CAMBODIA. Since our formation in 1984, we have quickly distinguished ourselves amongst our competitors by our innovative yet thoughtful design solutions.</p>
+
+									<div class="button">
+										<button>COMPANY PROFILE</button>
+									</div>
+
+								</div>
+							</div>
+							<div class="col-sm-6"></div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-sm-6"></div>
+					<div class="col-sm-6">
+						<div class="img">
+							<img src="assets/images/Architecture-Link-option21-overview-01.jpg">
+						</div>
+					</div>
+				</div>
+			</div>
+<style type="text/css">
+	#content .luxury{}
+	#content .luxury .group-luxury{padding-top: 80px;width: 82%;}
+	#content .luxury .title{margin-left: 67px;}
+	#content .luxury .title h4{margin: 0;font-size: 18px;text-transform: uppercase;letter-spacing: 7px;margin-top: 50px;}
+	#content .luxury .title h2{text-align: left;margin: 0;font-size: 35px;margin-top: 50px;padding-right: 64%;}
+	#content .luxury .box{margin-top: 20px;display: flex;}
+	#content .luxury .items{margin-top: 30px;overflow: hidden;display: flex;align-items: center;}
+	#content .luxury .items .img{height: 450px;width: 100%;overflow: hidden;position: relative;}
+	#content .luxury .items .img .gradientb{opacity: 0;}
+	#content .luxury .items .img img{width: 100%;height: 100%;object-fit: cover;}
+	#content .luxury .items .text{width: 100%;display: flex;justify-content: center;align-items: center;flex-direction: column;position: absolute;color: #fff;opacity: 0;transition: 0.7s;}
+	#content .luxury .items .text h3{font-size: 18px;}
+	#content .luxury .items .text p{font-size: 14px;}
+
+#content .luxury .zoom-out{overflow: hidden;}
+#content .luxury .zoom-out img{transition: 0.7s;}
+#content .luxury .zoom-out:hover img{-moz-transform: scale(1);-webkit-transform: scale(1.1);transform: scale(1.1);}
+#content .luxury .zoom-out:hover .text{opacity: 1;z-index: 2;}
+
+#content .luxury .box-thin-height .col-sm-4 .img{height: 290px;}
+#content .luxury .items:hover .gradientb{opacity: 1;z-index: 1;}
+
+</style>
+			<!-- Touch modern concepts and designs with luxury architecture. -->
+			<div class="container-fluid luxury">
+				<div class="container group-luxury">
+					<div class="title">
+						<div class="linehigh"></div>
+						<h4>LATEST PROJECTS</h4>
+						<h2>Touch modern concepts and designs with luxury architecture.</h2>
+					</div>
+					<div class="row">
+						<div class="box"></div>
+							<div class="col-sm-6">
+								<div class="items zoom-out">
+									<div class="img wow slideInUp slow" >
+										<div class="gradientb"></div>
+										<img src="assets/images/Architecture-portfolios-details-Link-option21-01.jpg">
+									</div>
+									<div class="text">
+										<div class="linehigh"></div>
+										<h3>OUR TAMPINES HUB</h3>
+										<p>Toul Kork - Phnom Penh</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="items zoom-out">
+									<div class="img wow slideInUp slow">
+										<div class="gradientb"></div>
+										<img src="assets/images/Architecture-portfolios-Link-option21-banner-01.jpg">
+									</div>
+									<div class="text">
+										<div class="linehigh"></div>
+										<h3>OUR TAMPINES HUB</h3>
+										<p>Toul Kork - Phnom Penh</p>
+									</div>
+								</div>
+							</div>
+							<div class="box-thin-height">
+								<div class="col-sm-4">
+									<div class="items zoom-out">
+										<div class="img wow slideInUp slow" >
+											<div class="gradientb"></div>
+											<img src="assets/images/Architecture-Link-option21-portfolios-04.jpg">
+										</div>
+										<div class="text">
+											<div class="linehigh"></div>
+											<h3>OUR TAMPINES HUB</h3>
+											<p>Toul Kork - Phnom Penh</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="items zoom-out">
+										<div class="img wow slideInUp slow">
+											<div class="gradientb"></div>
+											<img src="assets/images/Architecture-Link-option21-portfolios-08.jpg">
+										</div>
+										<div class="text">
+											<div class="linehigh"></div>
+											<h3>OUR TAMPINES HUB</h3>
+											<p>Toul Kork - Phnom Penh</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="items zoom-out">
+										<div class="img wow slideInUp slow">
+											<div class="gradientb"></div>
+											<img src="assets/images/Architecture-Link-option21-portfolios-05.jpg">
+										</div>
+										<div class="text">
+											<div class="linehigh"></div>
+											<h3>OUR TAMPINES HUB</h3>
+											<p>Toul Kork - Phnom Penh</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="items zoom-out">
+										<div class="img wow slideInUp slow">
+											<div class="gradientb"></div>
+											<img src="assets/images/Architecture-Link-option21-portfolios-06.jpg">
+										</div>
+										<div class="text">
+											<div class="linehigh"></div>
+											<h3>OUR TAMPINES HUB</h3>
+											<p>Toul Kork - Phnom Penh</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="items zoom-out">
+										<div class="img wow slideInUp slow">
+											<div class="gradientb"></div>
+											<img src="assets/images/Architecture-Link-option21-portfolios-07.jpg">
+										</div>
+										<div class="text">
+											<div class="linehigh"></div>
+											<h3>OUR TAMPINES HUB</h3>
+											<p>Toul Kork - Phnom Penh</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="items zoom-out">
+										<div class="img wow slideInUp slow">
+											<div class="gradientb"></div>
+											<img src="assets/images/Architecture-Link-option21-portfolios-03.jpg">
+										</div>
+										<div class="text">
+											<div class="linehigh"></div>
+											<h3>OUR TAMPINES HUB</h3>
+											<p>Toul Kork - Phnom Penh</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							
+					</div>
+
+				</div>
+			</div>
+<style type="text/css">
+	#content .profile{overflow: hidden;display: flex;justify-content: center;}
+	#content .profile .b-img{width: 100%;height: 100%;position: relative;}
+	#content .profile .b-img .left{}
+	#content .profile .b-img .right{background-image: url('assets/images/ptn-1.png');background-repeat: repeat;height: 900px;z-index: 10;}
+	/*#content .profile .group-profile{padding-top: 80px;width: 82%;position: absolute;}
+	#content .profile .title{margin-left: 67px;}
+	#content .profile .title h4{margin: 0;font-size: 18px;text-transform: uppercase;letter-spacing: 7px;margin-top: 50px;}
+	#content .profile .title h2{text-align: left;margin: 0;font-size: 35px;margin-top: 50px;padding-right: 54%;}*/
+
+#slider{position: absolute;width: 100%;}
+#slider img{width: 300px;height: 300px}
+</style>
+			<!-- We is making headlines for raising the profile of architects news in the world. -->
+			<div class="container-fluid profile">
+				<div class="b-img">
+					<div class="col-sm-6 left"></div>
+					<div class="col-sm-6 right"></div>
+				</div>
+				<!-- <div class="container group-profile">
+					<div class="title">
+						<div class="linehigh"></div>
+						<h4>LATEST NEWS & EVENTS</h4>
+						<h2>We is making headlines for raising the profile of architects news in the world.</h2>
+					</div>
+					<div class="row">
+						
+					</div>
+				</div> -->
+				<div id="slider">
+					<img src="assets/images/Architecture-Link-option21-media-01.jpg">
+					<img src="assets/images/Architecture-Link-option21-media-01.jpg">
+					<img src="assets/images/Architecture-Link-option21-media-01.jpg">
+					<img src="assets/images/Architecture-Link-option21-media-01.jpg">
+				</div>
+			</div>
+
+			
+
+
+
+<script type="text/javascript">
+	$('#slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+});
+</script>
+
+
+
+
+<style type="text/css">
+	#content .happy{}
+	#content .happy .group-happy{padding-top: 80px;width: 82%;}
+	#content .happy .title{margin-left: 67px;}
+	#content .happy .title h4{margin: 0;font-size: 18px;text-transform: uppercase;letter-spacing: 7px;margin-top: 50px;} 
+	#content .happy p{text-align: center;padding: 0 29%;margin: 0;line-height: 30px;font-size: 18px;margin-top: 50px;}
+</style>
+			<!-- H A P P Y C L I E N T ’ S S A Y -->
+			<div class="container-fluid happy">
+				<div class="container group-happy">
+					<div class="title">
+						<div class="linehigh"></div>
+						<h4>HAPPY CLIENT’S SAY</h4>
+					</div>
+						<div class="row">
+						<p>Thanks you all for your tremendous help designing lumiere hotel project. Without your diligence, hard workd, and several late night work, we would not have been able to meet our deadline. More than that Lumiere is always fully booked.</p>
+					</div>
+				</div>
+				
+			</div>
+<style type="text/css">
+#content .partner{display: flex;}
+#content .partner .group-partner{padding-top: 80px;width: 82%;}
+#content .partner .title{margin-left: 67px;}
+#content .partner .title h4{margin: 0;font-size: 18px;text-transform: uppercase;letter-spacing: 7px;margin-top: 50px;}
+}
+#partner.slick-initialized .slick-slide {display: flex !important;flex-direction: column;justify-content: center;align-items: center;}
+#partner .slick-slide>div {width: 170px;height: 80px;margin-top: 50px;}
+#partner .slick-slide>div>img {width: 100%; height: 100%;object-fit: cover;}
+#partner .slick-dots {position: absolute;display: inline-flex; width: calc(100% - 30px);padding-top: 40px; margin: 0;bottom: -40px;list-style: none;text-align: center; justify-content: center;}
+#partner .slick-dots li { padding: 5px;}
+#partner .slick-dots li button { width: 8px;height: 8px;background-color: white;border: red 1px solid;border-radius: 50%;padding: 0;}
+#partner .slick-dots li.slick-active button {background-color: red;}
+
+</style>
+			<!-- O U R P A R T N E R S & C L I E N T S -->
+			<div class="container-fluid partner">
+				<div class="container group-partner">
+					<div class="title">
+						<div class="linehigh"></div>
+						<h4>OUR PARTNERS & CLIENTS</h4>
+					</div>
+
+					<div id="partner">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+						<img src="assets/images/Architecture-TEAM-Link-option21-banner-01.jpg">
+
+
+					</div>
+				</div>
+			</div>
+		
+
+		</div>
+	</div>
+
+
+
+
+
+	<script type="text/javascript">
+		$('#partner').slick({
+    autoplay: true,
+    autoplaySpeed: 6000,
+    infinite: true,
+    arrows: false,
+    dots: true,
+    rows: 2,
+    slidesToShow: 8,
+    slidesToScroll: 8,
+  });
+
+	$('.slick-dots li button').text('');
+		
+	</script>
+	<style>
+
+
+</style>
---
+++ footer-desktop.php
@@ -0,0 +1,75 @@
+<div id="wrapper">
+		<div id="footer">
+
+<style type="text/css">
+
+#footer .display .title{display: flex;justify-content: center;flex-direction: column;align-items: center;}
+#footer .display .title h4{margin: 0;font-size: 18px;text-transform: uppercase;letter-spacing: 7px;margin-top: 50px;}
+#footer .display .items ul li{line-height: 25px;}
+#footer .display .items .dev-by{padding-top: 30px;}
+</style>		
+			<!-- DISPLAY SUITE OPEN -->
+			<div class="container-fluid display">
+				<div class="container group-display">
+					<div class="title">
+						<div class="linehigh"></div>
+						<h4>CONTACT US</h4>
+					</div>
+					<div class="row">
+						<div class="items">
+							<ul>
+								<li>45Z, 371, Trapeang Chhuk, Teuk Thla, Sen Sok, Phnom Penh, Cambodia</li>
+								<li><a href="#">+855 23 44 00 70 / +855 12 44 00 70 / +855 10 44 00 70</a></li>
+								<li><a href="#">info@archsite.com | www.archsite.com</a></li>
+							</ul>
+							<div class="dev-by">
+								<p>© 2018. All rights reserved. ARCHSITE, Sitemap</p>
+								<p><a href="#">Developed by: TomnerbTECH</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+
+
+		<script async defer id="github-bjs" src="https://buttons.github.io/buttons.js"></script>
+    <script type="text/javascript">
+        // object-fit polyfill run
+        objectFitImages();
+
+        /* init Jarallax */
+        jarallax(document.querySelectorAll('.jarallax'));
+
+        jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+            keepImg: true,
+        });
+
+        /* init Flickity Carousel + Jarallax inside it */
+        // new Flickity( '.demo-carousel', {
+        //     initialIndex: 1,
+        //     autoPlay: 3000,
+        //     pauseAutoPlayOnHover: false,
+        //     pageDots: false,
+        //     prevNextButtons: false,
+        //     wrapAround: true
+        // });
+        // jarallax(document.querySelectorAll('.demo-carousel .jarallax'), {
+        //     elementInViewport: document.querySelectorAll('.demo-carousel')
+        // });
+
+        /* init Jarallax with jQuery */
+        // $('.jarallax').jarallax({
+        //     speed: 0.5,
+        //     imgWidth: 1366,
+        //     imgHeight: 768
+        // });
+    </script>
+    <!-- JS -->
+	<!-- <script src="//code.jquery.com/jquery-3.2.1.min.js"></script> -->
+	<script src="<?php echo $BATH_URL;?>assets/scripts/jquery.fancybox.min.js"></script>
+</body>
+</html>
